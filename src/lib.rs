e2619e3dd5c3a21c5925f0c455dc401ed9572b06
Aditya Rajan <arajan@redhat.com>
2021-09-22 15:56:39 +0530

    network: Add types,serializer,deserializer for NetworkOptions
    Signed-off-by: Aditya Rajan <arajan@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
+#[macro_use]
+extern crate serde;
+extern crate serde_derive;
+extern crate serde_json;
+
+pub mod serialize;
+pub mod network;
 pub mod commands;
+
+impl network::NetworkOptions {
+    pub fn load(path: &str) -> Result<network::NetworkOptions, serialize::SerializeError> {
+        serialize::deserialize(path)
+    }
+    pub fn save(&self, path: &str) -> Result<(), serialize::SerializeError> {
+        serialize::serialize(self, path)
+    }
+}
