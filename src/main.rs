e335ca791c12e8afd69f346dd3d656e6b4ac5a54
Aditya Rajan <arajan@redhat.com>
2021-09-22 14:09:43 +0530

    cli: Integrate clap and bootstrap initial command structure
    Signed-off-by: Aditya Rajan <arajan@redhat.com>

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+
+use std::path::PathBuf;
+use clap::{crate_version, Clap};
+
+use netavark::commands::setup;
+use netavark::commands::teardown;
+
+#[derive(Clap, Debug)]
+#[clap(version = crate_version!())]
+struct Opts {
+    /// Instead of reading from STDIN, read the configuration to be applied from the given file.
+    #[clap(short, long)]
+    file: Option<PathBuf>,
+    /// Netavark trig command
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap, Debug)]
+enum SubCommand {
+    #[clap(version = crate_version!())]
+    /// Configures the given network namespace with the given configuration.
+    Setup(setup::Setup),
+    #[clap(version = crate_version!())]
+    /// Undo any configuration applied via setup command.
+    Teardown(teardown::Teardown),
+}
+
+fn main() {
+    let opts = Opts::parse();
+
+    match opts.subcmd {
+        SubCommand::Setup(setup) => setup.exec(opts.file.unwrap()),
+        SubCommand::Teardown(teardown) => teardown.exec(opts.file.unwrap()),
+    }
+}
