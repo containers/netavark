894fbc52e793144c9c3bf50c716cb5898b77adac
Lokesh Mandvekar <lsm5@fedoraproject.org>
2021-10-05 10:48:39 -0400

    add the log crate with sample debug message
    Fixes: #11
    
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- src/commands/teardown.rs
+++ src/commands/teardown.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 use clap::{self, Clap};
 use crate::network;
+use log::debug;
 
 #[derive(Clap, Debug)]
 pub struct Teardown {
@@ -18,12 +19,15 @@ impl Teardown {
     }
 
     pub fn exec(&self, input_file: PathBuf) {
+
+        debug!("{:?}", "Tearing down..");
         //TODO: Can we be more safe while converting PathBuf to string
         let _network_options = match network::NetworkOptions::load(&input_file.into_os_string().into_string().unwrap()) {
             Ok(opts) => opts,
             Err(e) => panic!("{}", e),
         };
 
+        debug!("{:?}", "Teardown complete");
         ()
     }
 }
