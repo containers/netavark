983097e0e80a1db9def8f4af72d3243e5ae5506a
Brent Baude <bbaude@redhat.com>
2022-07-14 12:21:50 -0500

    Use latest aardvark-dns binary
    Instead of using a packaged version of aardvark-dns, we grabbed the
    latest main branch zip copy and use it instead.
    
    Signed-off-by: Brent Baude <bbaude@redhat.com>

--- contrib/cirrus/setup.sh
+++ contrib/cirrus/setup.sh
@@ -28,3 +28,14 @@ if [[ "$CIRRUS_TASK_NAME" == "build_cross" ]]; then
 	# TODO: We can move this to quay.io if we reach rate-limits, hopefully that's not gonna happen for netavark
 	echo '  "rustembedded/cross" = "docker.io/rustembedded/cross"'  >> /etc/containers/registries.conf.d/000-shortnames.conf
 fi
+
+req_env_vars AARDVARK_DNS_URL
+
+set -x  # show what's happening
+curl --fail --location -o /tmp/aardvark-dns.zip "$AARDVARK_DNS_URL"
+mkdir -p /usr/libexec/podman
+cd /usr/libexec/podman
+rm -f aardvark-dns*
+unzip -o /tmp/aardvark-dns.zip
+chmod a+x /usr/libexec/podman/aardvark-dns
+
