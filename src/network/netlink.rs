cc1730ef34f7eb05039ea891bce1904a176147dc
Paul Holzinger <pholzing@redhat.com>
2022-11-23 17:10:42 +0100

    netlink: add set_link_ns
    Add a function to move an interface in a new netns.
    
    This will be needed by the example plugin which I will add in the
    folloing commits.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- src/network/netlink.rs
+++ src/network/netlink.rs
@@ -174,6 +174,16 @@ impl Socket {
         Ok(())
     }
 
+    pub fn set_link_ns(&mut self, link_id: u32, netns_fd: i32) -> NetavarkResult<()> {
+        let mut msg = LinkMessage::default();
+        msg.header.index = link_id;
+        msg.nlas.push(Nla::NetNsFd(netns_fd));
+
+        let result = self.make_netlink_request(RtnlMessage::SetLink(msg), NLM_F_ACK)?;
+        expect_netlink_result!(result, 0);
+        Ok(())
+    }
+
     fn create_addr_msg(link_id: u32, addr: &ipnet::IpNet) -> AddressMessage {
         let mut msg = AddressMessage::default();
         msg.header.index = link_id;
