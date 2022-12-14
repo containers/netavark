containers/netavark

commit 011f899919c9d702f950456f18c33dcc080259a0

release,make: add rule crate-publish for publishing crates.io
* Add new rule `make crate-publish` to automate publish at https://crates.io/crates/netavark
* Document steps for publishing to `crates.io`

Signed-off-by: Aditya R <arajan@redhat.com>

Makefile
@@ -10,6 +10,9 @@ LIBEXECDIR ?= ${PREFIX}/libexec
 LIBEXECPODMAN ?= ${LIBEXECDIR}/podman
 
 SELINUXOPT ?= $(shell test -x /usr/sbin/selinuxenabled && selinuxenabled && echo -Z)
+# Get crate version by parsing the line that starts with version.
+CRATE_VERSION ?= $(shell grep ^version Cargo.toml | awk '{print $$3}')
+GIT_TAG ?= $(shell git describe --tags)
 
 # Set this to any non-empty string to enable unoptimized
 # build w/ debugging features.
@@ -46,6 +49,16 @@ $(CARGO_TARGET_DIR):
 build: bin $(CARGO_TARGET_DIR)
 	cargo build $(release)
 	cp $(CARGO_TARGET_DIR)/$(profile)/netavark bin/netavark$(if $(debug),.debug,)
+
+.PHONY: crate-publish
+crate-publish:
+	@if [ "$(CRATE_VERSION)" != "$(GIT_TAG)" ]; then\
+		echo "Git tag is not equivalent to the version set in Cargo.toml. Please checkout the correct tag";\
+		exit 1;\
+	fi
+	@echo "It is expected that you have already done 'cargo login' before running this command. If not command may fail later"
+	cargo publish --dry-run
+	cargo publish
 
 .PHONY: clean
 clean:

commit 97e90a303051d19469ac474b8877620b1354168f

fix make docs
Regression caused by 011f899919c9. We only want to build actual man
pages of course and not general docs.
This commit fixes the wildcard target to only use the `.1.md` files
instead of all markdown files.
Also add the step to the validate target to make sure CI tests this
target.

Fixes #524

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

Makefile
@@ -101,6 +101,7 @@ integration: $(CARGO_TARGET_DIR)
 validate: $(CARGO_TARGET_DIR)
 	cargo fmt --all -- --check
 	cargo clippy -p netavark -- -D warnings
+	$(MAKE) docs
 
 .PHONY: vendor-tarball
 vendor-tarball: build install.cargo-vendor-filterer

docs/Makefile
@@ -3,7 +3,7 @@ DATADIR ?= ${PREFIX}/share
 MANDIR ?= $(DATADIR)/man
 MANDOWN ?= $(shell export PATH; command -v mandown)
 
-docs: $(patsubst %.md,%,$(wildcard *.md))
+docs: $(patsubst %.md,%,$(wildcard *.1.md))
 
 %.1: %.1.md
 	$(MANDOWN) $< > $@