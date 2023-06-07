72aa0ddb470162aaad99dc801a0726c9ec02e30d
Paul Holzinger <pholzing@redhat.com>
2023-06-07 19:06:47 +0200

    dhcp-proxy: apply new ip address/gateway
    If we get a new lease with different ips or a new gateway we have to
    update the contianer netns with the new addresses. The added tests takes
    over 2m because the minimal lease time that dnsmasq supports is 2m so we
    have to love with that for now.
    
    One outstanding issue is that podman has no idea that things changed, it
    will continue to show incorrect network info in podman inspect. This
    never worked with CNI either so it should be ok for now.
    
    However I think it would be a great improvement for long running
    containers if we could somehow update the satus in podman. I think we
    need some hidden podman command callback where we can feed podman the
    new info.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- src/dhcp_proxy/dhcp_service.rs
+++ src/dhcp_proxy/dhcp_service.rs
@@ -1,8 +1,14 @@
+use std::net::Ipv4Addr;
+
 use crate::dhcp_proxy::dhcp_service::DhcpServiceErrorKind::{
     Bug, InvalidArgument, NoLease, Timeout,
 };
 
 use crate::dhcp_proxy::lib::g_rpc::{Lease as NetavarkLease, NetworkConfig};
+use crate::error::{ErrorWrap, NetavarkError, NetavarkResult};
+use crate::network::core_utils;
+use crate::network::netlink::Route;
+use crate::wrap;
 use log::debug;
 use mozim::{DhcpV4ClientAsync, DhcpV4Config, DhcpV4Lease as MozimV4Lease};
 use tokio_stream::StreamExt;
@@ -36,7 +42,7 @@ impl DhcpServiceError {
 pub struct DhcpV4Service {
     client: DhcpV4ClientAsync,
     network_config: NetworkConfig,
-    previous_lease: Option<NetavarkLease>,
+    previous_lease: Option<MozimV4Lease>,
 }
 
 impl DhcpV4Service {
@@ -63,7 +69,7 @@ impl DhcpV4Service {
     ///
     pub async fn get_lease(&mut self) -> Result<NetavarkLease, DhcpServiceError> {
         if let Some(Ok(lease)) = self.client.next().await {
-            let mut netavark_lease = <NetavarkLease as From<MozimV4Lease>>::from(lease);
+            let mut netavark_lease = <NetavarkLease as From<MozimV4Lease>>::from(lease.clone());
             netavark_lease.add_domain_name(&self.network_config.domain_name);
             netavark_lease.add_mac_address(&self.network_config.container_mac_addr);
 
@@ -71,7 +77,7 @@ impl DhcpV4Service {
                 "found a lease for {:?}, {:?}",
                 &self.network_config.container_mac_addr, &netavark_lease
             );
-            self.previous_lease = Some(netavark_lease.clone());
+            self.previous_lease = Some(lease);
 
             return Ok(netavark_lease);
         }
@@ -104,20 +110,34 @@ pub async fn process_client_stream(mut client: DhcpV4Service) {
     while let Some(lease) = client.client.next().await {
         match lease {
             Ok(lease) => {
-                log::debug!(
+                log::info!(
                     "got new lease for mac {}: {:?}",
                     &client.network_config.container_mac_addr,
                     &lease
                 );
-                let lease = NetavarkLease::from(lease);
                 // get previous lease and check if ip addr changed, if not we do not have to do anything
                 if let Some(old_lease) = &client.previous_lease {
                     if old_lease.yiaddr != lease.yiaddr
-                        || old_lease.gateways != lease.gateways
                         || old_lease.subnet_mask != lease.subnet_mask
+                        || old_lease.gateways != lease.gateways
                     {
                         // ips do not match, remove old ones and assign new ones.
-                        log::error!("ips do not match, reassign not implemented")
+                        log::info!(
+                            "ip or gateway for mac {} changed, update address",
+                            &client.network_config.container_mac_addr
+                        );
+                        match update_lease_ip(
+                            &client.network_config.ns_path,
+                            &client.network_config.container_iface,
+                            old_lease,
+                            &lease,
+                        ) {
+                            Ok(_) => {}
+                            Err(err) => {
+                                log::error!("{err}");
+                                continue;
+                            }
+                        }
                     }
                 }
                 client.previous_lease = Some(lease)
@@ -128,4 +148,66 @@ pub async fn process_client_stream(mut client: DhcpV4Service) {
             ),
         }
     }
+}
+
+fn update_lease_ip(
+    netns: &str,
+    interface: &str,
+    old_lease: &MozimV4Lease,
+    new_lease: &MozimV4Lease,
+) -> NetavarkResult<()> {
+    let (_, netns) =
+        core_utils::open_netlink_sockets(netns).wrap("failed to open netlink socket in netns")?;
+    let mut sock = netns.netlink;
+    let old_net = wrap!(
+        ipnet::Ipv4Net::with_netmask(old_lease.yiaddr, old_lease.subnet_mask),
+        "create ipnet from old lease"
+    )?;
+    let new_net = wrap!(
+        ipnet::Ipv4Net::with_netmask(new_lease.yiaddr, new_lease.subnet_mask),
+        "create ipnet from new lease"
+    )?;
+
+    if new_net != old_net {
+        let link = sock
+            .get_link(crate::network::netlink::LinkID::Name(interface.to_string()))
+            .wrap("get interface in netns")?;
+        sock.add_addr(link.header.index, &ipnet::IpNet::V4(new_net))
+            .wrap("add new addr")?;
+        sock.del_addr(link.header.index, &ipnet::IpNet::V4(old_net))
+            .wrap("remove old addrs")?;
+    }
+    if new_lease.gateways != old_lease.gateways {
+        if let Some(gws) = &old_lease.gateways {
+            let old_gw = gws.first();
+            if let Some(gw) = old_gw {
+                let route = Route::Ipv4 {
+                    dest: ipnet::Ipv4Net::new(Ipv4Addr::new(0, 0, 0, 0), 0)?,
+                    gw: *gw,
+                    metric: None,
+                };
+                match sock.del_route(&route) {
+                    Ok(_) => {}
+                    Err(err) => match err.unwrap() {
+                        // special case do not error if route does not exists
+                        NetavarkError::Netlink(e) if -e.code == libc::ESRCH => {}
+                        _ => return Err(err).wrap("delete old default route"),
+                    },
+                };
+            }
+        }
+        if let Some(gws) = &new_lease.gateways {
+            let new_gw = gws.first();
+            if let Some(gw) = new_gw {
+                let route = Route::Ipv4 {
+                    dest: ipnet::Ipv4Net::new(Ipv4Addr::new(0, 0, 0, 0), 0)?,
+                    gw: *gw,
+                    metric: None,
+                };
+                sock.add_route(&route)?;
+            }
+        }
+    }
+
+    Ok(())
 }
