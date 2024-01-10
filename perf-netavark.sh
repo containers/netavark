61a990fe7f75b1ee537cbea21f85c55f6940267e
Paul Holzinger <pholzing@redhat.com>
2024-01-10 18:49:48 +0100

    perf-netavark: fix missing --config arg
    --config is now required so we have to set it here as well.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- perf-netavark.sh
+++ perf-netavark.sh
@@ -7,9 +7,11 @@ trap cleanup EXIT
 
 function cleanup() {
     kill -9 $netnspid
+    rm -rf $TMP_CONFIG
 }
 
+TMP_CONFIG=$(mktemp -d)
 unshare -n sleep 100 &
 netnspid=$!
 
-unshare -n perf stat $NETAVARK -f ./test/testfiles/simplebridge.json setup /proc/$netnspid/ns/net
+unshare -n perf stat $NETAVARK -f ./test/testfiles/simplebridge.json --config $TMP_CONFIG setup /proc/$netnspid/ns/net
