468f8854c855495ed30aa34ed2298cfbebc7997d
Paul Holzinger <pholzing@redhat.com>
2024-01-10 18:53:31 +0100

    perf-netavark: accept fw driver as argument
    Makes testing with different drivers easier.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- perf-netavark.sh
+++ perf-netavark.sh
@@ -14,4 +14,9 @@ TMP_CONFIG=$(mktemp -d)
 unshare -n sleep 100 &
 netnspid=$!
 
+# first arg is the fw driver
+if [ -n "$1" ]; then
+    export NETAVARK_FW="$1"
+fi
+
 unshare -n perf stat $NETAVARK -f ./test/testfiles/simplebridge.json --config $TMP_CONFIG setup /proc/$netnspid/ns/net
