d6294050fb2ea47bc88fb68a393667e92ddf7c90
Paul Holzinger <pholzing@redhat.com>
2025-04-10 14:05:18 +0200

    firewalld_reload: share connection
    There is no reason to reopen a new dbus conneciton here every time when
    we get the signal, we can do everything on one connection.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- src/commands/firewalld_reload.rs
+++ src/commands/firewalld_reload.rs
@@ -33,40 +33,40 @@ pub fn listen(config_dir: Option<OsString>) -> NetavarkResult<()> {
         .cache_properties(CacheProperties::No)
         .build()?;
 
+    let conn_option = Some(conn);
+
     // Setup fw rules on start because we are started after firewalld
     // this means at the time firewalld stated the fw rules were flushed
     // and we need to add them back.
     // It is important to keep things like "systemctl restart firewalld" working.
-    reload_rules(config_dir);
+    reload_rules(config_dir, &conn_option);
 
     // This loops forever until the process is killed or there is some dbus error.
     for _ in proxy.0.receive_signal(SIGNAL_NAME)? {
         log::debug!("got firewalld {} signal", SIGNAL_NAME);
-        reload_rules(config_dir);
+        reload_rules(config_dir, &conn_option);
     }
 
     Ok(())
 }
 
-fn reload_rules(config_dir: &Path) {
-    if let Err(e) = reload_rules_inner(config_dir) {
+fn reload_rules(config_dir: &Path, conn: &Option<Connection>) {
+    if let Err(e) = reload_rules_inner(config_dir, conn) {
         log::error!("failed to reload firewall rules: {e}");
     }
 }
 
-fn reload_rules_inner(config_dir: &Path) -> NetavarkResult<()> {
+fn reload_rules_inner(config_dir: &Path, conn: &Option<Connection>) -> NetavarkResult<()> {
     let conf = read_fw_config(config_dir).wrap("read firewall config")?;
     // If we got no conf there are no containers so nothing to do.
     if let Some(conf) = conf {
         let fw_driver = get_supported_firewall_driver(Some(conf.driver))?;
 
-        let dbus_conn = zbus::blocking::Connection::system().ok();
-
         for net in conf.net_confs {
-            fw_driver.setup_network(net, &dbus_conn)?;
+            fw_driver.setup_network(net, conn)?;
         }
         for port in &conf.port_confs {
-            fw_driver.setup_port_forward(port.into(), &dbus_conn)?;
+            fw_driver.setup_port_forward(port.into(), conn)?;
         }
         log::info!("Successfully reloaded firewall rules");
     }
