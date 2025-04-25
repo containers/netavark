containers/netavark

commit a08bb557fd606e12cc5b6452df0e88fbc544fe67

Makefile: do not rebuild if nothing changed
If the src has not changed there is no need to rebuild.
This removes the -debug suffix from the binary if build with debug set,
I doubt anyone actually uses this and we need the same name for the
bin/netavark target to actually work. This is even better because now
you could build a debug binary and run the test with it without changing
the test path manually.

Fixes #1178

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

Makefile
@@ -29,6 +29,8 @@ CARGO ?= cargo
 CARGO_TARGET_DIR ?= targets
 export CARGO_TARGET_DIR  # 'cargo' is sensitive to this env. var. value.
 
+SOURCES = src/** Cargo.toml Cargo.lock
+
 ifdef debug
 $(info debug is $(debug))
   # These affect both $(CARGO_TARGET_DIR) layout and contents
@@ -50,10 +52,13 @@ $(CARGO_TARGET_DIR):
 	mkdir -p $@
 
 .PHONY: build
-build: bin $(CARGO_TARGET_DIR)
+build: bin/netavark
+
+bin/netavark: $(SOURCES) bin $(CARGO_TARGET_DIR)
 	$(CARGO) build $(release)
-	cp $(CARGO_TARGET_DIR)/$(profile)/netavark bin/netavark$(if $(debug),.debug,)
-	cp $(CARGO_TARGET_DIR)/$(profile)/netavark-dhcp-proxy-client bin/netavark-dhcp-proxy-client$(if $(debug),.debug,)
+	cp $(CARGO_TARGET_DIR)/$(profile)/netavark bin/netavark
+	cp $(CARGO_TARGET_DIR)/$(profile)/netavark-dhcp-proxy-client bin/netavark-dhcp-proxy-client
+
 
 .PHONY: examples
 examples: bin $(CARGO_TARGET_DIR)

commit 7084b2e4364c8dd83f9162dfac17f5b877b29e2b

Makefile: correctly set SOURCES
Use find to list all source files recursively, the ** doesn't actually
seem to work correctly in all cases. It requires the use the the
globstar shell option which may or may not be set.

While at it also add Makefile so we rebuild if the Makefile is changed,
e.g. when adding new flags to cargo.

Fixes: a08bb557fd ("Makefile: do not rebuild if nothing changed")

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

Makefile
@@ -29,7 +29,7 @@ CARGO ?= cargo
 CARGO_TARGET_DIR ?= targets
 export CARGO_TARGET_DIR  # 'cargo' is sensitive to this env. var. value.
 
-SOURCES = src/** Cargo.toml Cargo.lock
+SOURCES = $(shell find src/ -type f) Cargo.toml Cargo.lock Makefile
 
 ifdef debug
 $(info debug is $(debug))