containers/netavark

commit 9a73a52c5fd1f20e327a01e29e7599beca668fc1

fix port forward with strict RPF and multi networks
As it turns out our routing setup doesn't play nice with strict Reverse
Path Forwarding. The issue is that the incoming packages are routed in
via one bridge but may be routed out from the container via another
bridge and thus get dropped by strict filtering. The fix is simple, set
the filter to loose mode.

From the sysctl doc:
  If using asymmetric routing or other complicated routing,
  then loose mode is recommended.

This applies to us so it seems like the proper fix. Added tests to
ensure it works.

Fixes https://issues.redhat.com/browse/RHEL-32500

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

src/network/bridge.rs
@@ -570,6 +570,17 @@ fn create_interfaces(
                     CoreUtils::apply_sysctl_value(br_accept_ra, "0")?;
                 }
 
+                // Disable strict reverse path search validation. On RHEL it is set to strict mode
+                // which breaks port forwarding when multiple networks are attached as the package
+                // may be routed over a different interface on the reverse path.
+                // As documented for the sysctl for complicated or asymmetric routing loose mode (2)
+                // is recommended.
+                let br_rp_filter = format!(
+                    "/proc/sys/net/ipv4/conf/{}/rp_filter",
+                    &data.bridge_interface_name
+                );
+                CoreUtils::apply_sysctl_value(br_rp_filter, "2")?;
+
                 let link = host
                     .get_link(netlink::LinkID::Name(
                         data.bridge_interface_name.to_string(),
@@ -690,6 +701,13 @@ fn create_veth_pair<'fd>(
             &data.container_interface_name
         );
         core_utils::CoreUtils::apply_sysctl_value(enable_arp_notify, "1")?;
+
+        // disable strict reverse path search validation
+        let rp_filter = format!(
+            "/proc/sys/net/ipv4/conf/{}/rp_filter",
+            &data.container_interface_name
+        );
+        CoreUtils::apply_sysctl_value(rp_filter, "2")?;
         Ok::<(), NetavarkError>(())
     });
     // check the result and return error

test/250-bridge-nftables.bats
@@ -696,6 +696,8 @@ EOF
     # when the sysctl value is already set correctly we should not error
     run_in_host_netns sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
     run_in_container_netns sh -c "echo 1 > /proc/sys/net/ipv4/conf/default/arp_notify"
+    run_in_host_netns sh -c "echo 2 > /proc/sys/net/ipv4/conf/default/rp_filter"
+    run_in_container_netns sh -c "echo 2 > /proc/sys/net/ipv4/conf/default/rp_filter"
     run_in_host_netns mount -t proc -o ro,nosuid,nodev,noexec proc /proc
 
     run_netavark --file ${TESTSDIR}/testfiles/simplebridge.json setup $(get_container_netns_path)

test/100-bridge-iptables.bats
@@ -554,6 +554,35 @@ fw_driver=iptables
     test_port_fw ip=6 proto=udp hostip="fd65:8371:648b:0c06::1"
 }
 
+# Test that port forwarding works with strict Reverse Path Forwarding enabled on the host
+@test "$fw_driver - port forwarding with two networks and RPF - tcp" {
+    # First, enable strict RPF on host/container ns.
+    run_in_host_netns sysctl -w net.ipv4.conf.all.rp_filter=1
+    run_in_host_netns sysctl -w net.ipv4.conf.default.rp_filter=1
+    run_in_container_netns sysctl -w net.ipv4.conf.all.rp_filter=1
+    run_in_container_netns sysctl -w net.ipv4.conf.default.rp_filter=1
+
+    # We need a dummy interface with a host ip,
+    # if we connect directly to the bridge ip it doesn't reproduce.
+    add_dummy_interface_on_host dummy0 "10.0.0.1/24"
+
+    run_netavark --file ${TESTSDIR}/testfiles/two-networks.json setup $(get_container_netns_path)
+    result="$output"
+
+    run_in_host_netns cat /proc/sys/net/ipv4/conf/podman2/rp_filter
+    assert "2" "rp_filter podman2 bridge"
+    run_in_host_netns cat /proc/sys/net/ipv4/conf/podman3/rp_filter
+    assert "2" "rp_filter podman3 bridge"
+
+    run_in_container_netns cat /proc/sys/net/ipv4/conf/eth0/rp_filter
+    assert "2" "rp_filter eth0 interface"
+    run_in_container_netns cat /proc/sys/net/ipv4/conf/eth1/rp_filter
+    assert "2" "rp_filter eth1 interface"
+
+    # Important: Use the "host" ip here and not localhost or bridge ip.
+    run_nc_test "0" "tcp" 8080 "10.0.0.1" 8080
+}
+
 @test "bridge ipam none" {
            read -r -d '\0' config <<EOF
 {
@@ -789,6 +818,8 @@ EOF
     # when the sysctl value is already set correctly we should not error
     run_in_host_netns sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
     run_in_container_netns sh -c "echo 1 > /proc/sys/net/ipv4/conf/default/arp_notify"
+    run_in_host_netns sh -c "echo 2 > /proc/sys/net/ipv4/conf/default/rp_filter"
+    run_in_container_netns sh -c "echo 2 > /proc/sys/net/ipv4/conf/default/rp_filter"
     run_in_host_netns mount -t proc -o ro,nosuid,nodev,noexec proc /proc
 
     run_netavark --file ${TESTSDIR}/testfiles/simplebridge.json setup $(get_container_netns_path)

commit e483f8a8c4abad835ec493f37418cc45c7f2d061

write bridge sysctl to config file
When writing directly to /proc/sys we have the risk that the system
later overwrites the values again, for systemd distros systemd-sysctl
seems to manage that and reapplies sysctls based of the sysctl.d config
files.

Now as RHEL ships a config file that set rp_filter = 1 by default this
means that our rp_filter = 2 could be overwritten again by it.
So in short the original fix to set rp_filter = 2 never worked actually
as the value got overwritten again.

So to actually fix this create a sysctl.d config file for the bridge
with all its required sysctl settings. However we connot only do that,
as this happens async systemd might have no set the value by the time we
start the container process. So to avoid any reaces we do both.
Also of course we should only do that when systemd is in use and when
run as root, as rootless we operate in our own private netns so systemd
doesn't see the interfaces there and we do not have the privileges to
write to /run either.

Fixes: 9a73a52 ("fix port forward with strict RPF and multi networks")
Fixes: https://issues.redhat.com/browse/RHEL-89477

Signed-off-by: Paul Holzinger <pholzing@redhat.com>

src/network/bridge.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::{collections::HashMap, net::IpAddr, os::fd::BorrowedFd, sync::Once};
 
 use ipnet::IpNet;
@@ -26,7 +27,7 @@ use super::{
         NO_CONTAINER_INTERFACE_ERROR, OPTION_HOST_INTERFACE_NAME, OPTION_ISOLATE, OPTION_METRIC,
         OPTION_MODE, OPTION_MTU, OPTION_NO_DEFAULT_ROUTE, OPTION_VLAN, OPTION_VRF,
     },
-    core_utils::{self, get_ipam_addresses, join_netns, parse_option, CoreUtils},
+    core_utils::{self, get_ipam_addresses, is_using_systemd, join_netns, parse_option, CoreUtils},
     driver::{self, DriverInfo},
     internal_types::{
         IPAMAddresses, IsolateOption, PortForwardConfig, SetupNetwork, TearDownNetwork,
@@ -174,6 +175,20 @@ impl driver::NetworkDriver for Bridge<'_> {
 
         let (host_sock, netns_sock) = netlink_sockets;
 
+        // Create sysctl writer, when using systemd and running as root it is possible that the
+        // global configured sysctl.d config conflict with the values we write. In such cases
+        // systemd-sysctl might overright the value causing hard to find errors:
+        // https://issues.redhat.com/browse/RHEL-89477
+        // This writer is used to generate a matching sysctl.d config file for our values so systemd is aware of them.
+        let mut sysctl_writer = if is_using_systemd() && !self.info.rootless {
+            let _ = fs::create_dir("/run/sysctl.d");
+            sysctl::SysctlDWriter::new(sysctl::get_bridge_sysctl_d_path(
+                &data.bridge_interface_name,
+            ))
+        } else {
+            None
+        };
+
         let container_veth_mac = create_interfaces(
             host_sock,
             netns_sock,
@@ -181,6 +196,7 @@ impl driver::NetworkDriver for Bridge<'_> {
             self.info.network.internal,
             self.info.netns_host,
             self.info.netns_container,
+            &mut sysctl_writer,
         )?;
 
         //  StatusBlock response
@@ -306,6 +322,9 @@ impl driver::NetworkDriver for Bridge<'_> {
                 self.setup_firewall(data)?
             }
         }
+        if let Some(w) = sysctl_writer {
+            w.commit();
+        }
 
         Ok((response, aardvark_entry))
     }
@@ -346,6 +365,19 @@ impl driver::NetworkDriver for Bridge<'_> {
         };
 
         if !self.info.network.internal && mode == BridgeMode::Managed {
+            if complete_teardown {
+                // delete sysctl file as well
+                let path = sysctl::get_bridge_sysctl_d_path(&bridge_name);
+                if let Err(e) = fs::remove_file(&path) {
+                    if e.kind() != std::io::ErrorKind::NotFound {
+                        error_list.push(NetavarkError::wrap(
+                            format!("failed to remove {path}"),
+                            e.into(),
+                        ));
+                    }
+                };
+            }
+
             match self.teardown_firewall(complete_teardown, bridge_name) {
                 Ok(_) => {}
                 Err(err) => {
@@ -584,6 +616,7 @@ fn create_interfaces(
     internal: bool,
     hostns_fd: BorrowedFd<'_>,
     netns_fd: BorrowedFd<'_>,
+    sysctl_writer: &mut Option<sysctl::SysctlDWriter<String>>,
 ) -> NetavarkResult<String> {
     let (bridge_index, mac) = match host.get_link(netlink::LinkID::Name(
         data.bridge_interface_name.to_string(),
@@ -638,8 +671,8 @@ fn create_interfaces(
                         format!("net/ipv6/conf/{}/accept_dad", &data.bridge_interface_name);
                     let br_accept_ra =
                         format!("net/ipv6/conf/{}/accept_ra", &data.bridge_interface_name);
-                    sysctl::apply_sysctl_value(br_accept_dad, "0")?;
-                    sysctl::apply_sysctl_value(br_accept_ra, "0")?;
+                    sysctl::apply_sysctl_value_with_writer(br_accept_dad, "0", sysctl_writer)?;
+                    sysctl::apply_sysctl_value_with_writer(br_accept_ra, "0", sysctl_writer)?;
                 }
 
                 // Disable strict reverse path search validation. On RHEL it is set to strict mode
@@ -649,7 +682,7 @@ fn create_interfaces(
                 // is recommended.
                 let br_rp_filter =
                     format!("net/ipv4/conf/{}/rp_filter", &data.bridge_interface_name);
-                sysctl::apply_sysctl_value(br_rp_filter, "2")?;
+                sysctl::apply_sysctl_value_with_writer(br_rp_filter, "2", sysctl_writer)?;
 
                 let link = host
                     .get_link(netlink::LinkID::Name(

src/network/sysctl.rs
@@ -1,18 +1,32 @@
 use std::{
-    fs::{File, OpenOptions},
-    io::{Error, ErrorKind, Read as _, Write as _},
+    fs::{self, File, OpenOptions},
+    io::{ErrorKind, Read as _, Result, Write as _},
+    path::Path,
 };
 
 use crate::error::{NetavarkError, NetavarkResult};
 
 pub fn apply_sysctl_value(ns_value: impl AsRef<str>, val: impl AsRef<str>) -> NetavarkResult<()> {
-    _apply_sysctl_value(&ns_value, val)
+    _apply_sysctl_value(&ns_value, val, &mut None::<SysctlDWriter<&str>>)
+        .map_err(|e| NetavarkError::wrap(format!("set sysctl {}", ns_value.as_ref()), e.into()))
+}
+
+pub fn apply_sysctl_value_with_writer(
+    ns_value: impl AsRef<str>,
+    val: impl AsRef<str>,
+    writer: &mut Option<SysctlDWriter<impl AsRef<Path>>>,
+) -> NetavarkResult<()> {
+    _apply_sysctl_value(&ns_value, val, writer)
         .map_err(|e| NetavarkError::wrap(format!("set sysctl {}", ns_value.as_ref()), e.into()))
 }
 
 /// Set a sysctl value by value's namespace.
 /// ns_value is the path of the sysctl (using slashes not dots!) and without the "/proc/sys/" prefix.
-fn _apply_sysctl_value(ns_value: impl AsRef<str>, val: impl AsRef<str>) -> Result<(), Error> {
+fn _apply_sysctl_value(
+    ns_value: impl AsRef<str>,
+    val: impl AsRef<str>,
+    sysctld_writer: &mut Option<SysctlDWriter<impl AsRef<Path>>>,
+) -> Result<()> {
     const PREFIX: &str = "/proc/sys/";
     let ns_value = ns_value.as_ref();
     let mut path = String::with_capacity(PREFIX.len() + ns_value.len());
@@ -21,6 +35,10 @@ fn _apply_sysctl_value(ns_value: impl AsRef<str>, val: impl AsRef<str>) -> Resul
     let val = val.as_ref();
 
     log::debug!("Setting sysctl value for {} to {}", ns_value, val);
+
+    if let Some(writer) = sysctld_writer {
+        writer.write(ns_value, val)?;
+    }
 
     let mut f = File::open(&path)?;
     let mut buf = String::with_capacity(1);
@@ -36,7 +54,11 @@ fn _apply_sysctl_value(ns_value: impl AsRef<str>, val: impl AsRef<str>) -> Resul
 
 pub fn disable_ipv6_autoconf(if_name: &str) -> NetavarkResult<()> {
     // make sure autoconf is off, we want manual config only
-    if let Err(err) = _apply_sysctl_value(format!("net/ipv6/conf/{if_name}/autoconf"), "0") {
+    if let Err(err) = _apply_sysctl_value(
+        format!("net/ipv6/conf/{if_name}/autoconf"),
+        "0",
+        &mut None::<SysctlDWriter<&str>>,
+    ) {
         match err.kind() {
             ErrorKind::NotFound => {
                 // if the sysctl is not found we likely run on a system without ipv6
@@ -55,4 +77,102 @@ pub fn disable_ipv6_autoconf(if_name: &str) -> NetavarkResult<()> {
         }
     };
     Ok(())
+}
+
+pub fn get_bridge_sysctl_d_path(bridge_name: &str) -> String {
+    format!("/run/sysctl.d/10-netavark-{bridge_name}.conf")
+}
+
+pub struct SysctlDWriter<P: AsRef<Path>> {
+    f: File,
+    path: P,
+    commit: bool,
+}
+
+impl<P: AsRef<Path>> SysctlDWriter<P> {
+    /// create a new sysctl writer for the given file, returns None if unable to write the file.
+    /// Important one must call commit() otherwise we remove the file again on drop.
+    pub fn new(path: P) -> Option<Self> {
+        // Note using create_new here as we don't want to overwrite an existing file
+        // If such file already exists it should be safe to assume it has the right
+        // content so we can skip writing it again.
+        let mut f = File::create_new(&path).ok()?;
+        f.write_all("# autogenerated by netavark\n".as_bytes())
+            .ok()?;
+        Some(Self {
+            f,
+            path,
+            commit: false,
+        })
+    }
+
+    /// write the sysctl in format according to sysctl.d(5)
+    /// Important one must call commit() otherwise we remove the file again on drop.
+    fn write(&mut self, ns_value: &str, val: &str) -> Result<()> {
+        let mut entry = String::with_capacity(ns_value.len() + val.len() + 4);
+        entry.push_str(ns_value);
+        entry.push_str(" = ");
+        entry.push_str(val);
+        entry.push('\n');
+        self.f.write_all(entry.as_bytes())?;
+        Ok(())
+    }
+
+    /// mark as successfully written so the file is not removed on drop
+    /// The reason we do this is to avoid manually removing the file on
+    /// each early exit due errors.
+    /// This also consumes the writer so it is impossible to keep writing
+    /// after this is called.
+    pub fn commit(mut self) {
+        self.commit = true;
+    }
+}
+
+impl<P: AsRef<Path>> Drop for SysctlDWriter<P> {
+    fn drop(&mut self) {
+        if self.commit {
+            // do nothing
+            return;
+        }
+        // ignore errors here
+        let _ = fs::remove_file(&self.path);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::Builder;
+
+    #[test]
+    fn test_sysctl_d_writer() {
+        let tmp_dir = Builder::new()
+            .prefix("example")
+            .tempdir()
+            .expect("failed to create tmpdir");
+        let filepath = tmp_dir.path().join("test.conf");
+        let mut w = SysctlDWriter::new(&filepath).expect("failed to get writer");
+        w.write("abc", "1").expect("failed to write value 1");
+        w.write("def", "2").expect("failed to write value 2");
+        w.commit();
+        let res = fs::read_to_string(filepath).expect("failed to read file");
+        assert_eq!(res, "# autogenerated by netavark\nabc = 1\ndef = 2\n");
+    }
+
+    #[test]
+    fn test_sysctl_d_writer_nocommit() {
+        let tmp_dir = Builder::new()
+            .prefix("example")
+            .tempdir()
+            .expect("failed to create tmpdir");
+        let filepath = tmp_dir.path().join("test.conf");
+        let mut w = SysctlDWriter::new(&filepath).expect("failed to get writer");
+        w.write("abc", "1").expect("failed to write value 1");
+        // drop the writer here which should delete the file
+        drop(w);
+        assert!(
+            !filepath.exists(),
+            "file should not exist after drop without commit()"
+        )
+    }
 }

test/250-bridge-nftables.bats
@@ -828,11 +828,25 @@ EOF
     done
 
     run_netavark setup $(get_container_netns_path) <<<"${configs[0]}"
+
+    local sysctl_content="# autogenerated by netavark
+net/ipv4/conf/podman1/rp_filter = 2"
+    run_in_host_netns cat /run/sysctl.d/10-netavark-podman1.conf
+    assert "$output" == "$sysctl_content" "sysctl.d file content"
+
     run_netavark setup $(get_container_netns_path 1) <<<"${configs[1]}"
+
+    # content must still be the same after second setup
+    run_in_host_netns cat /run/sysctl.d/10-netavark-podman1.conf
+    assert "$output" == "$sysctl_content" "sysctl.d file content"
 
     run_netavark teardown $(get_container_netns_path) <<<"${configs[0]}"
     # bridge should still exist
     run_in_host_netns ip link show podman1
+
+    # after first teardown file must still exist as bridge is still active
+    run_in_host_netns cat /run/sysctl.d/10-netavark-podman1.conf
+    assert "$output" == "$sysctl_content" "sysctl.d file content"
 
     # check nftables POSTROUTING chain
     run_in_host_netns nft list chain inet netavark POSTROUTING
@@ -857,6 +871,9 @@ EOF
     run_netavark teardown $(get_container_netns_path 1) <<<"${configs[1]}"
     # bridge should be removed
     expected_rc=1 run_in_host_netns ip link show podman1
+
+    # after all bridge teardown file must be removed
+    expected_rc=1 run_in_host_netns cat /run/sysctl.d/10-netavark-podman1.conf
 
     run_in_host_netns nft list chain inet netavark FORWARD
     assert "${lines[3]}" =~ "ct state invalid drop" "forward rule 1"

test/helpers.bash
@@ -113,7 +113,10 @@ function teardown() {
 function create_netns() {
     # create a new netns and mountns and run a sleep process to keep it alive
     # we have to redirect stdout/err to /dev/null otherwise bats will hang
-    unshare -nm sleep inf &>/dev/null &
+    unshare -nm --propagation private sleep inf &>/dev/null &
+    # netavark writes to /run/sysctl.d, mount a tmpfs to not leak stuff on the host
+    mkdir -p /run/sysctl.d
+    nsenter -n -m -w -t $! mount -t tmpfs none /run/sysctl.d
     echo $!
 }
 