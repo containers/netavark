1db5fd2e64bb778264a454e2cb699dc3a3a562f9
Paul Holzinger <pholzing@redhat.com>
2025-07-30 17:12:58 +0200

    update tonic and prost to 0.14
    Some new crates we have to use but not actually code changes.
    
    Signed-off-by: Paul Holzinger <pholzing@redhat.com>

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 use std::process::Command;
 
 fn main() {
-    let builder = tonic_build::configure()
+    let builder = tonic_prost_build::configure()
         .type_attribute("netavark_proxy.Lease", "#[derive(serde::Serialize)]")
         .type_attribute("netavark_proxy.DhcpV4Lease", "#[derive(serde::Serialize)]")
         .type_attribute("netavark_proxy.DhcpV6Lease", "#[derive(serde::Serialize)]")
